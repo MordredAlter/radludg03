be3692cdbb97faf9e8c8e1a16173006c2c03fd44
MordredAlter <39255157+MordredAlter@users.noreply.github.com>
2019-02-15 00:45:36 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // We need 3 things everytime we use Three.js
  // Scene + Camera + Renderer
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 )
 const renderer = new THREE.WebGLRenderer({ antialias: true})
 
 renderer.setSize( window.innerWidth, window.innerHeight )
@@ -26,12 +26,20 @@ var cube = new THREE.Mesh ( geometry, material )
 scene.add( cube )
 
 // wireframe cube
-var geometry = new THREE.BoxGeometry( 1, 1, 1)
+var geometry = new THREE.BoxGeometry( 1.1, 1.1, 1.1)
 var material = new THREE.MeshBasicMaterial( {
-	color: "#dadada", wireframe: true, transparent: true
+	color: "#272dd1", wireframe: true, transparent: true
 })
 var wireframeCube = new THREE.Mesh ( geometry, material )
 scene.add( wireframeCube )
+
+//2do wireframe cube
+var geometry2 = new THREE.BoxGeometry( 1.2, 1.2, 1.2)
+var material2 = new THREE.MeshBasicMaterial( {
+	color: "#272dd1", wireframe: true, transparent: true
+})
+var wireframeCube2 = new THREE.Mesh ( geometry2, material2 )
+scene.add( wireframeCube2 )
 
 // ambient light
 var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.2)
@@ -45,10 +53,12 @@ scene.add( pointLight );
 
 function animate() {
 	requestAnimationFrame( animate )
-	cube.rotation.x += 0.06;
-	cube.rotation.y += 0.06;
-	wireframeCube.rotation.x -= 0.06;
-	wireframeCube.rotation.y -= 0.06;
+	cube.rotation.x += 1;
+	cube.rotation.y += 1;
+	wireframeCube.rotation.x -= 0.04;
+	wireframeCube.rotation.y -= 0.04;
+	wireframeCube2.rotation.x += 0.04;
+	wireframeCube2.rotation.y += 0.04;
 	renderer.render( scene, camera )
 }
 animate()
